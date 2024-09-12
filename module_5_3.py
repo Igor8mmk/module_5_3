68c432c360e1c0b8e7ad7737cd4aca82a79f927d
Igor8mmk <igor.mmk1@gmail.com>
2024-09-12 13:06:57 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -18,22 +18,41 @@ class House():
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors == other
+
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors <= other
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors > other
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors >= other
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        if isinstance(other, int):
+            return self.number_of_floors != other
 
     def __add__(self, other):
         if isinstance(other, House):
